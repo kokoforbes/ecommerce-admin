aba8e75f973a1b2cca00b9014d614309c5cd2033
Abdullahi onikoko <onikokoabdullahi@gmail.com>
2025-01-13 12:40:01 +0000

    Added zustand for state management

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 
 const SetupPage = () => {
   return (
     <div className='p-4'>
-      <UserButton />
+      <Modal
+        title='test'
+        description='test description'
+        isOpen
+        onClose={() => {}}
+      >
+        children
+      </Modal>
     </div>
   );
 };
