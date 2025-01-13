a14f86bcba87e9ff003c2683e01b743ed5083f5f
Abdullahi onikoko <onikokoabdullahi@gmail.com>
2025-01-13 13:12:57 +0000

    created form for store creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ModalProvider } from "@/providers/modal-provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ModalProvider />
           {children}
         </body>
       </html>
