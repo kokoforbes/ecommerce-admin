4e5d29613b53d76370df56e8cca924bb939bbd0d
Abdullahi onikoko <onikokoabdullahi@gmail.com>
2025-01-14 01:16:29 +0000

    Created Billboard page and components

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -24,7 +24,7 @@ const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
   }
 
   return (
-    <div className='flex-col mx-auto max-w-7xl'>
+    <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
         <SettingsForm initialData={store} />
       </div>
